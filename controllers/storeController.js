47a73ef823a2c8925fb25c7004888167e11caf42
ΔNDRΞW <drewdrummer88@gmail.com>
2017-05-12 02:40:19 -0500

    GUESS WHO DIDNT COMMIT AGAIN. finished video 10/44 now. Learned about database storage. setting up the schema. created store model. learned about saving stores, using mixins, and creating the first mixin form for reuse. populated form with fields from schema. next is async/await and sending data to mongo

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,13 @@
 exports.homePage = (req, res) => {
+	console.log(req.name);
 	res.render('index');	
+};
+
+exports.addStore = (req, res) => {
+	res.render('editStore', {title: 'Add Store' });
+}
+
+
+exports.createStore = (req, res) => {
+	res.json(req.body);
 };
